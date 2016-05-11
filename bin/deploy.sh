effc7173feb543f27f53a01ccfb60ce9a3b8cb93
KamataRyo <mugil.cephalus+github@gmail.com>
2016-05-11 13:02:30 +0900

    Fix Travis settings

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,7 +7,9 @@ if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
 	exit
 fi
 
-if ! [[ "master" == "$TRAVIS_BRANCH" || "$TRAVIS_BRANCH" =~ ^v?[0-9](\.[0-9])* ]]; then
+TAG_PATTERN=^v?[0-9](\.[0-9])*
+
+if ! [[ "master" == "$TRAVIS_BRANCH" || "$TRAVIS_BRANCH" =~ $TAG_PATTERN ]]; then
 	echo "Not on the 'master' branch or tags."
 	exit
 fi
@@ -35,16 +37,21 @@ git config user.name "kamataryo"
 git config user.email "kamataryo@travis-ci.org"
 git add .
 
+
 if [[ "master" == "$TRAVIS_BRANCH" ]]; then
+
 	echo "deploy as 'latest'"
+
 	git commit --quiet -m "Deploy from travis"
 	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:latest > /dev/null 2>&1
 fi
 
-if [[ "$TRAVIS_BRANCH" =~ ^v?[0-9](\.[0-9])* ]]; then
-	tag=${TRAVIS_BRANCH}-released
-	echo "deploy as tagged '$tag'"
+if [[ "$TRAVIS_BRANCH" =~ $TAG_PATTERN ]]; then
+
+	TAG_NAME=${TRAVIS_BRANCH}-release
+	echo "deploy as tagged '$TAG_NAME'"
+
 	git commit --quiet -m "Deploy from travis"
-	git tag $tag
+	git tag $TAG_NAME
 	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" --tags > /dev/null 2>&1
 fi
