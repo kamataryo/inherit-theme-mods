3221a72435c5dc7bd80d1b55b3169a6cfe3adb84
KamataRyo <mugil.cephalus+github@gmail.com>
2016-05-14 22:08:36 +0900

    Update travis setting

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,20 +2,25 @@
 
 set -e
 
-if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+if ! [[ "$WP_VERSION"         == "$WP_VERSION_TO_DEPLOY" && \
+        "$TRAVIS_PHP_VERSION" == "$PHP_VERSION_TO_DEPLOY" && \
+		"$WP_MULTISITE"       == "$WP_MULTISITE_TO_DEPLOY" ]]; then
+	echo "Not deploying for PHP=$TRAVIS_PHP_VERSION & WP=$WP_VERSION";
+	exit
+elif [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
 	echo "Not deploying pull requests."
 	exit
+elif ! [[ "master" == "$TRAVIS_BRANCH" ]]; then
+	echo "Not on the 'master' branch."
+	if [[ "" == "$TRAVIS_TAG" ]]; then
+		echo "Not tagged."
+		exit
+	fi
 fi
 
-
-if ! [[ "master" == "$TRAVIS_BRANCH" || "$TRAVIS_BRANCH" =~ ^v?[0-9](\.[0-9])* ]]; then
-	echo "Not on the 'master' branch or tags."
-	exit
-fi
+COMMIT_MESSAGE=$(git log --format=%B -n 1 "$TRAVIS_COMMIT")
 
 rm -rf .git
-rm -r .gitignore
-
 echo "README.md
 bin
 .travis.yml
@@ -36,21 +41,18 @@ git config user.name "kamataryo"
 git config user.email "kamataryo@travis-ci.org"
 git add .
 
-
 if [[ "master" == "$TRAVIS_BRANCH" ]]; then
-
-	echo "deploy as 'latest'"
-
-	git commit --quiet -m "Deploy from travis"
+	echo "deploy on 'latest' branch, tested on PHP=$TRAVIS_PHP_VERSION & WP=$WP_VERSION"
+	git commit --quiet -m "Deploy from travis
+	Original commit is $TRAVIS_COMMIT."
 	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:latest > /dev/null 2>&1
 fi
 
-if [[ "$TRAVIS_BRANCH" =~ ^v?[0-9](\.[0-9])* ]]; then
-
-	TAG_NAME=${TRAVIS_BRANCH}-release
-	echo "deploy as tagged '$TAG_NAME'"
-
-	git commit --quiet -m "Deploy from travis"
-	git tag $TAG_NAME
+if ! [[  "" == "$TRAVIS_TAG" ]]; then
+	echo "deploy as tagged '$TRAVIS_TAG'"
+	git commit --quiet -m "Deploy from travis, tested on PHP=$TRAVIS_PHP_VERSION & WP=$WP_VERSION"
+	git tag "$TRAVIS_TAG" -m "$COMMIT_MESSAGE
+	Original commit is $TRAVIS_COMMIT."
 	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" --tags > /dev/null 2>&1
 fi
+exit 0
