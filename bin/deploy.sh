46a5aac31d9739e6c2365b1ceee1dd3bd7fc14d7
KamataRyo <mugil.cephalus+github@gmail.com>
2016-05-14 22:41:55 +0900

    Update message on Travis:

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,7 +5,7 @@ set -e
 if ! [[ "$WP_VERSION"         == "$WP_VERSION_TO_DEPLOY" && \
         "$TRAVIS_PHP_VERSION" == "$PHP_VERSION_TO_DEPLOY" && \
 		"$WP_MULTISITE"       == "$WP_MULTISITE_TO_DEPLOY" ]]; then
-	echo "Not deploying for this matrix";
+	echo "Not deploying from this matrix";
 	exit
 elif [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
 	echo "Not deploying pull requests."
@@ -15,7 +15,8 @@ elif ! [[ "master" == "$TRAVIS_BRANCH" ]]; then
 	if [[ "" == "$TRAVIS_TAG" ]]; then
 		echo "Not tagged."
 		exit
-	fi
+	else
+	echo "tagged."
 fi
 
 COMMIT_MESSAGE=$(git log --format=%B -n 1 "$TRAVIS_COMMIT")
@@ -43,16 +44,15 @@ git add .
 
 if [[ "master" == "$TRAVIS_BRANCH" ]]; then
 	echo "deploy on 'latest' branch, tested on PHP=$TRAVIS_PHP_VERSION & WP=$WP_VERSION"
-	git commit --quiet -m "Deploy from travis
-	Original commit is $TRAVIS_COMMIT."
+	git commit --quiet -m "Deploy from travis.\nOriginal commit is $TRAVIS_COMMIT."
 	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:latest > /dev/null 2>&1
 fi
 
 if ! [[  "" == "$TRAVIS_TAG" ]]; then
 	echo "deploy as tagged '$TRAVIS_TAG'"
-	git commit --quiet -m "Deploy from travis, tested on PHP=$TRAVIS_PHP_VERSION & WP=$WP_VERSION"
-	git tag "$TRAVIS_TAG" -m "$COMMIT_MESSAGE
-	Original commit is $TRAVIS_COMMIT."
+	git commit --quiet -m"Deploy from travis, tested on PHP=$TRAVIS_PHP_VERSION & WP=$WP_VERSION"
+	git tag "$TRAVIS_TAG" -m"$COMMIT_MESSAGE\nOriginal commit is $TRAVIS_COMMIT."
 	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" --tags > /dev/null 2>&1
 fi
+
 exit 0
