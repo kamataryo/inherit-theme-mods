38acbf9492e1f9ba4902b7a2edb61b6a0b6eab50
kamataryo <kamataryo@travis-ci.org>
2016-06-12 07:57:32 +0000

    Deploy from travis.
    Original commit is 0bbebbe451cc013197874db95a551d69a96ba7e5.

---
+++ inherit-theme-mods-ui.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * @package inherit-theme-mods
+ */
+
+/**
+ * Control I/O of this plugin via UI
+ */
+class Inherit_Theme_Mods_UI {
+	// nonce for this plugin
+	const NONCE_ACTION = 'inherit_theme_mods_nonce_action';
+	// default capability to access this plugin and its functions
+	const CAPABILITY   = 'manage_options';
+	static private $ajax_actions = array(
+		'inherit' => 'ITM_inherit',
+		'restore' => 'ITM_restore',
+		'overwrite' => 'ITM_overwrite',
+	);
+	private $itm;
+
+	function __construct() {
+		$this->itm = new Inherit_Theme_Mods();
+		add_action( 'plugins_loaded', array( $this, 'register_textdomain' ) );
+		add_action( 'admin_menu',     array( $this, 'register_admin_menu' ) );
+		add_action( 'wp_ajax_' . self::$ajax_actions['inherit'],   array( $this, 'ajax_inherit' ) );
+		add_action( 'wp_ajax_' . self::$ajax_actions['overwrite'], array( $this, 'ajax_overwrite' ) );
+		add_action( 'wp_ajax_' . self::$ajax_actions['restore'],   array( $this, 'ajax_restore' ) );
+	}
+
+	function register_textdomain() {
+		load_plugin_textdomain(
+			'inherit-theme-mods',
+			false,
+			basename( dirname( __FILE__ ) ) . '/languages'
+		);
+	}
+
+	function register_admin_menu() {
+		$hook = add_options_page(
+			__( 'Inherit Theme Mods', 'inherit-theme-mods' ),
+			__( 'Inherit Theme Mods', 'inherit-theme-mods' ),
+			self::CAPABILITY,
+			'inherit-theme-mods',
+			array( $this, 'describe_ui' )
+		);
+		add_action( "admin_head-$hook", array( $this, 'enqueue_scripts' ) );
+	}
+
+	function enqueue_scripts() {
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script(
+			'itm_script',
+			ITM_Util::url( 'assets', 'inherit-theme-mods.js' ),
+			array( 'jquery' )
+		);
+		wp_enqueue_style(
+			'itm_style',
+			ITM_Util::url( 'assets', 'inherit-theme-mods.css' ),
+			array()
+		);
+		wp_localize_script( 'itm_script', 'ajax', array(
+			'endpoint' => admin_url( 'admin-ajax.php' ),
+			'nonce'    => wp_create_nonce( self::NONCE_ACTION ),
+			'status'   => array(
+				'success'      => __( 'Processed successfully.', 'inherit-theme-mods' ) . '<a href="' . esc_url( home_url( '/' ) ) . '" class="ITM-aside">' . __( 'Visit Site', 'inherit-theme-mods' ) . '</a>',
+				'unknownError' => __( 'Unknown error', 'inherit-theme-mods' ),
+				'updating'     => __( 'Updating..', 'inherit_theme_mods', 'inherit-theme-mods' ),
+			),
+		) );
+	}
+
+	private function ajax( $action ) {
+		$message = $this->check_ajax_not_acceptable( $action );
+		if ( ! $message ) {
+			// $this->itm->$action(); # >PHP5.4
+			call_user_func( array( $this->itm, $action ) ); # PHP5.3
+			$data = $this->get_new_table()->data;
+			foreach ($data as $index => $datum ) {
+				unset( $data[$index]['key'] );
+				unset( $data[$index]['Key'] );
+			}
+			wp_send_json_success( $data ); # xss OK
+		} else {
+			wp_send_json_error( esc_html( $message ) );
+		}
+	}
+
+	public function ajax_inherit()   { $this->ajax( 'inherit' ); }
+
+	public function ajax_overwrite() { $this->ajax( 'overwrite' ); }
+
+	public function ajax_restore()   { $this->ajax( 'restore' ); }
+
+	public function describe_ui() {
+		?>
+		<div id="ITM" class="wrap">
+			<h1 id="ITM-title"><?php _e( 'Inherit Theme Mods', 'inherit-theme-mods' ); ?>
+				<span id="ITM-instant-notifier" class="ITM-status-notifier ITM-aside"></span>
+			</h1>
+
+			<?php
+			$itm = new Inherit_Theme_Mods();
+			if ( ! $itm->is_child_theme_active() ) { # PHP5.3
+				?>
+				<div id="ITM-notifier" class="notice notice-warning">
+					<p>
+						<?php _e( 'The active theme is not child theme. To use this plugin, activate child theme. This plugin is simply working as inspector now.', 'inherit-theme-mods' ); ?>
+					</p>
+				</div>
+				<?php
+				$this->describe_list_table_area();
+			} else {
+				$this->describe_header_area();
+				$this->describe_list_table_area();
+			}
+			?>
+		</div><!--#ITM-->
+		<?php
+	}
+
+	static function describe_header_area() {
+		?>
+		<div id="ITM-notifier" class="ITM-visit-site notice notice-success">
+			<p><?php _e( "This is a plugin to inherit parent theme's properties to child.", 'inherit-theme-mods' ); ?></p>
+		</div>
+
+		<form class="ITM-form">
+
+			<h2 class="ITM-action-header"><?php  _e( 'Inherit Properties', 'inherit-theme-mods' ); ?></h2>
+			<p><?php _e( "Copy and inherit parent theme's properties to child. Original child properties will be preserved. The last child properties are stored at trash box once for backup.", 'inherit-theme-mods' ); ?></p>
+			<a id="ITM-inherit" class="ITM-button button button-primary button-large" data-action="<?php echo esc_attr( self::$ajax_actions['inherit']); ?>">
+				<?php _e( 'inherit', 'inherit-theme-mods' ); ?>
+			</a>
+
+			<h2 class="ITM-action-header"><?php  _e( 'Overwrite Properties', 'inherit-theme-mods' ); ?></h2>
+			<p><?php _e( "Copy and overwrite parent theme's properties to child. Original child properties will be aborted. The last child properties are stored at trash box once for backup.", 'inherit-theme-mods' ); ?></p>
+			<a id="ITM-overwrite" class="ITM-button button button-primary button-large" data-action="<?php echo esc_attr( self::$ajax_actions['overwrite']); ?>">
+				<?php _e( 'overwrite', 'inherit-theme-mods' ); ?>
+			</a>
+
+
+			<h2 class="ITM-action-header"><?php  _e( 'Restore Properties', 'inherit-theme-mods' ); ?></h2>
+			<p><?php _e( "Restore child properties from trash box.", 'inherit-theme-mods' ); ?></p>
+			<a id="ITM-restore" class="ITM-button button button-primary button-large" data-action="<?php echo esc_attr( self::$ajax_actions['restore']); ?>">
+				<?php _e( 'restore', 'inherit-theme-mods' ); ?>
+			</a>
+		</form>
+		<?php
+	}
+
+	// generate list table with Admin Table class
+	function describe_list_table_area() {
+
+		$itmTable = $this->get_new_table();
+		$itmTable->prepare_items();
+		echo '<div id="ITM-Content" class="wrap">';
+		$itmTable->display();
+		echo '</div>';
+	}
+
+	// refresh table
+	function get_new_table() {
+		return new Inherit_Theme_Mods_Table(
+			$this->itm->child_theme_slug,
+			$this->itm->parent_theme_slug
+		);
+	}
+
+	static function verify_nonce() {
+		if ( func_num_args() > 0 ) {
+			return wp_verify_nonce( func_get_arg( 0 ) , self::NONCE_ACTION );
+		} elseif ( isset( $_REQUEST['nonce'] ) ) {
+			return wp_verify_nonce( $_REQUEST['nonce'] , self::NONCE_ACTION );
+		} else {
+			return false;
+		}
+	}
+
+	private function check_ajax_not_acceptable( $method ) {
+		if ( ! current_user_can( self::CAPABILITY ) ) {
+			return __( 'You do not have sufficient permissions for the request.', 'inherit-theme-mods' );
+
+		} else if ( ! self::verify_nonce() ) {
+			return __( 'Invalid request.', 'inherit-theme-mods' );
+
+		} else if ( ! $this->itm->is_child_theme_active() ) {
+			return sprintf( __( 'No Child Theme has been activated.', 'inherit-theme-mods' ) );
+		} else {
+			return false;
+		}
+	}
+}
---
+++ inherit-theme-mods.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * @package inherit-theme-mods
+ */
+
+/**
+* Wrapper for Options API
+*/
+class Inherit_Theme_Mods {
+	// prefix for theme mods defined by WordPress
+	const OPTION_PREFIX = 'theme_mods_';
+	// option name to store mods in trash box
+	const STORE_KEY = 'inherit_theme_mods_stored_option';
+	public $parent_theme_slug;
+	public $child_theme_slug;
+
+	function __construct() {
+		$this->parent_theme_slug = wp_get_theme()->template;
+		$this->child_theme_slug  = wp_get_theme()->stylesheet;
+	}
+
+	// This substitutes get_theme_mod.
+	static function get_theme_mods_of( $slug ) {
+		$result = get_option( self::OPTION_PREFIX . $slug, false );
+		return ( '' === $result ) ? array() : $result;
+	}
+
+	// This substitutes set_theme_mod.
+	static function set_theme_mods_of( $slug, $values ) {
+		return self::is_installed_theme( $slug ) ?
+			update_option( self::OPTION_PREFIX . $slug, $values ) : false;
+	}
+
+	// This substitutes set_theme_mod and merges options.
+	static function merge_theme_mods_of( $slug, $overwriter ) {
+
+		$overwritten = get_option( self::OPTION_PREFIX . $slug );
+		if (! is_array( $overwritten ) ) {
+			$overwritten = array();
+		}
+		return self::is_installed_theme( $slug ) ?
+			self::set_theme_mods_of(
+				$slug,
+				array_merge( $overwritten, $overwriter )
+			) : false;
+	}
+
+	// substitutes get_theme_mod, virtually for 'trash box'
+	static function get_stored_mods(){
+		return get_option( self::STORE_KEY, false );
+	}
+
+	static function is_installed_theme( $slug ) {
+		return array_key_exists( $slug, wp_get_themes() );
+	}
+
+	public function is_child_theme_active() {
+		return $this->parent_theme_slug !== $this->child_theme_slug;
+	}
+
+	// inherit theme mods from parent into child
+	public function inherit() {
+
+		if ( $this->is_child_theme_active() ) {
+
+			// store first
+			$this->store();
+			// inherit
+			return self::merge_theme_mods_of(
+				$this->child_theme_slug,
+				self::get_theme_mods_of( $this->parent_theme_slug )
+			);
+
+		} else {
+			return false;
+		}
+	}
+
+	// overwrite theme mods from parent into child
+	public function overwrite() {
+
+		if ( $this->is_child_theme_active() ) {
+
+			// store first
+			$this->store();
+			// overwrite
+			return self::set_theme_mods_of(
+				$this->child_theme_slug,
+				self::get_theme_mods_of( $this->parent_theme_slug )
+			);
+
+		} else {
+			return false;
+		}
+	}
+
+	// store child theme mods into trash box
+	public function store() {
+
+		if ( $this->is_child_theme_active() ) {
+
+			delete_option( self::STORE_KEY );
+			return add_option(
+				self::STORE_KEY,
+				self::get_theme_mods_of( $this->child_theme_slug ),
+				'',
+				'no'
+			);
+
+		} else {
+			return false;
+		}
+	}
+
+	// restore child theme mods from trash box
+	public function restore() {
+
+		if ( $this->is_child_theme_active() ) {
+
+			return self::set_theme_mods_of(
+				$this->child_theme_slug,
+				self::get_stored_mods()
+			);
+		} else {
+			return false;
+		}
+	}
+}
